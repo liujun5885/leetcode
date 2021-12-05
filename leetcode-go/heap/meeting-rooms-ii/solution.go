db4a5b42e21967bfdc0a6cc82aa616f403cd8ddc
liujun <liujun@rxthinking.com>
2021-12-05 09:50:13 +0800

    https://leetcode-cn.com/problems/copy-list-with-random-pointer/submissions/

--- leetcode-go/heap/meeting-rooms-ii/solution.go
+++ leetcode-go/heap/meeting-rooms-ii/solution.go
@@ -1,5 +1,40 @@
+// Package meeting_rooms_ii https://leetcode-cn.com/problems/meeting-rooms-ii/
 package meeting_rooms_ii
 
+import (
+	"container/heap"
+	"sort"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int            { return len(h) }
+func (h IntHeap) Less(i, j int) bool  { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
 func minMeetingRooms(intervals [][]int) int {
-	return 0
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	h := &IntHeap{}
+	for _, i := range intervals {
+		if len(*h) == 0 {
+			heap.Push(h, i[1])
+			continue
+		}
+		if (*h)[0] <= i[0] {
+			heap.Pop(h)
+		}
+		heap.Push(h, i[1])
+	}
+	return len(*h)
 }
--- leetcode-go/heap/meeting-rooms-ii/solution_test.go
+++ leetcode-go/heap/meeting-rooms-ii/solution_test.go
@@ -6,9 +6,27 @@ import (
 )
 
 func TestCase01(t *testing.T) {
-	intervals := [][]int{{0, 30}, {5, 10}, {15, 20}}
+	intervals := [][]int{{5, 10}, {15, 20}, {0, 30}}
 	actual := minMeetingRooms(intervals)
 	expected := 2
+	if !cmp.Equal(actual, expected) {
+		t.Errorf("actual: %v != expected: %v", actual, expected)
+	}
+}
+
+func TestCase02(t *testing.T) {
+	intervals := [][]int{{6, 15}, {13, 20}, {6, 17}}
+	actual := minMeetingRooms(intervals)
+	expected := 3
+	if !cmp.Equal(actual, expected) {
+		t.Errorf("actual: %v != expected: %v", actual, expected)
+	}
+}
+
+func TestCase03(t *testing.T) {
+	intervals := [][]int{{13, 15}, {1, 13}}
+	actual := minMeetingRooms(intervals)
+	expected := 1
 	if !cmp.Equal(actual, expected) {
 		t.Errorf("actual: %v != expected: %v", actual, expected)
 	}
