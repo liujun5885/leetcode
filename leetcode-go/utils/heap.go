d1e41ff9188c380ba9998c8674234bd5c6e89009
liujun <liujun@rxthinking.com>
2021-12-05 10:02:59 +0800

    https://leetcode-cn.com/problems/copy-list-with-random-pointer/submissions/

--- leetcode-go/utils/heap.go
+++ leetcode-go/utils/heap.go
@@ -1,1 +1,16 @@
 package utils
+
+func Heapify(h []int) {
+
+}
+
+func HeapPush(h []int, v int) {
+}
+
+func HeapPop(h []int, v int) {
+
+}
+
+func HeapPeek(h []int) {
+
+}
---
+++ leetcode-go/utils/heap_test.go
@@ -0,0 +1,15 @@
+package utils
+
+import "testing"
+
+func TestHeapify(t *testing.T) {
+
+}
+
+func TestHeapPush(t *testing.T) {
+
+}
+
+func TestHeapPop(t *testing.T) {
+
+}
