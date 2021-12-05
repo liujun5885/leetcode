19865f8baee46f969c8969a42b4483d24b41f7b9
liujun <liujun@rxthinking.com>
2021-12-05 09:57:20 +0800

    https://leetcode-cn.com/problems/copy-list-with-random-pointer/submissions/

---
+++ leetcode-rust/heap/src/meeting_rooms_ii.rs
@@ -0,0 +1,40 @@
+// https://leetcode-cn.com/problems/meeting-rooms-ii/
+
+use priority_queue::PriorityQueue;
+
+struct Solution;
+impl Solution {
+    pub fn min_meeting_rooms(intervals: Vec<Vec<i32>>) -> i32 {
+        let mut new_intervals = intervals.to_vec();
+        new_intervals.sort_by(|x, y| x[0].cmp(&y[0]));
+
+        let mut pq = PriorityQueue::new();
+        for i in new_intervals {
+            if pq.is_empty() {
+                pq.push(i[1], -1 * i[1]);
+                continue;
+            }
+            if let Some(v) = pq.peek() {
+                if v.0 < &i[1] {
+                    pq.pop();
+                }
+                pq.push(i[1], -1 * i[1]);
+            }
+        }
+
+        pq.len() as i32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::meeting_rooms_ii::Solution;
+
+    #[test]
+    fn case01() {
+        let intervals = vec![vec![0, 30], vec![5, 10], vec![15, 20]];
+        let actual = Solution::min_meeting_rooms(intervals);
+        let expected = 2;
+        assert_eq!(actual, expected)
+    }
+}
