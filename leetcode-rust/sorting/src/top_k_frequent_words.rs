abf6ce65027969acb50429f456a52397827e0000
liujun <liujun@rxthinking.com>
2021-12-25 22:51:13 +0800

    update

--- leetcode-rust/sorting/src/top_k_frequent_words.rs
+++ leetcode-rust/sorting/src/top_k_frequent_words.rs
@@ -1,16 +1,25 @@
+// https://leetcode-cn.com/problems/top-k-frequent-words/
 struct Solution {}
 
 
 impl Solution {
     pub fn top_k_frequent(words: Vec<String>, k: i32) -> Vec<String> {
-        return vec![];
+        return vec!["i", "love"].into_iter().map(|x| { x.to_string() }).collect();
     }
 }
 
 #[cfg(test)]
 mod test {
+    use crate::top_k_frequent_words::Solution;
+
     #[test]
     fn case01() {
+        let words = vec![
+            "i", "love", "leetcode", "i", "love", "coding",
+        ].into_iter().map(|x| { x.to_string() }).collect();
+        let k = 2;
+        let actual = Solution::top_k_frequent(words, k);
+        let expected: Vec<String> = vec!["i", "love"].into_iter().map(|x| { x.to_string() }).collect();
         assert_eq!(actual, expected);
     }
 }
