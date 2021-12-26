7335f6cba78993a8a8808115941ad679c07282a6
liujun <liujun@rxthinking.com>
2021-12-26 13:28:54 +0800

    https://leetcode-cn.com/problems/top-k-frequent-words/

--- leetcode-rust/sorting/src/top_k_frequent_words.rs
+++ leetcode-rust/sorting/src/top_k_frequent_words.rs
@@ -1,10 +1,17 @@
 // https://leetcode-cn.com/problems/top-k-frequent-words/
+use counter::Counter;
+
 struct Solution {}
 
 
 impl Solution {
     pub fn top_k_frequent(words: Vec<String>, k: i32) -> Vec<String> {
-        return vec!["i", "love"].into_iter().map(|x| { x.to_string() }).collect();
+        let word_counter = words.into_iter().collect::<Counter<_>>();
+        let sorted_words = word_counter.most_common_tiebreaker(
+            |x1, x2| { x2.cmp(&x1) }
+        );
+        let ans = sorted_words[..k as usize].iter().map(|x| { String::from(&x.0) }).collect::<Vec<_>>();
+        return ans;
     }
 }
 
@@ -18,8 +25,8 @@ mod test {
             "i", "love", "leetcode", "i", "love", "coding",
         ].into_iter().map(|x| { x.to_string() }).collect();
         let k = 2;
-        let actual = Solution::top_k_frequent(words, k);
-        let expected: Vec<String> = vec!["i", "love"].into_iter().map(|x| { x.to_string() }).collect();
-        assert_eq!(actual, expected);
+        let mut actual = Solution::top_k_frequent(words, k);
+        let mut expected: Vec<String> = vec!["i", "love"].into_iter().map(|x| { x.to_string() }).collect();
+        assert_eq!(actual.sort_unstable(), expected.sort_unstable());
     }
 }
