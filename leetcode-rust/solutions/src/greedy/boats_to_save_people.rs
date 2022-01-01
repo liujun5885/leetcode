7d58a79ab8df0c44bf3117a01354814c0c96f44a
liujun <liujun@rxthinking.com>
2022-01-01 09:53:16 +0800

    https://leetcode-cn.com/problems/valid-triangle-number/

---
+++ leetcode-rust/solutions/src/greedy/boats_to_save_people.rs
@@ -0,0 +1,79 @@
+struct Solution;
+
+impl Solution {
+    pub fn num_rescue_boats(people: Vec<i32>, limit: i32) -> i32 {
+        let mut sorted_people = people.clone();
+        let mut ans = 0;
+        sorted_people.sort();
+        let mut light = 0;
+        let mut heavy = sorted_people.len() - 1;
+
+        while light < heavy {
+            if sorted_people[heavy] > limit {
+                return 0;
+            }
+            if sorted_people[heavy] + sorted_people[light] > limit {
+                heavy -= 1;
+            } else {
+                light += 1;
+                heavy -= 1;
+            }
+            ans += 1;
+        }
+        if light == heavy {
+            ans += 1;
+        }
+
+        return ans;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::greedy::boats_to_save_people::Solution;
+
+    #[test]
+    fn case01() {
+        let people = vec![1, 2];
+        let limit = 3;
+        let actual = Solution::num_rescue_boats(people, limit);
+        let expected = 1;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn case02() {
+        let people = vec![3, 2, 2, 1];
+        let limit = 3;
+        let actual = Solution::num_rescue_boats(people, limit);
+        let expected = 3;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn case03() {
+        let people = vec![3, 5, 3, 4];
+        let limit = 5;
+        let actual = Solution::num_rescue_boats(people, limit);
+        let expected = 4;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn case04() {
+        let people = vec![1, 2, 3, 4];
+        let limit = 5;
+        let actual = Solution::num_rescue_boats(people, limit);
+        let expected = 2;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn case05() {
+        let people = vec![1, 2, 3, 4, 5];
+        let limit = 6;
+        let actual = Solution::num_rescue_boats(people, limit);
+        let expected = 3;
+        assert_eq!(actual, expected);
+    }
+}
--- leetcode-rust/solutions/src/greedy/mod.rs
+++ leetcode-rust/solutions/src/greedy/mod.rs
@@ -3,3 +3,4 @@ mod jump_game_ii;
 mod best_time_to_buy_and_sell_stock_ii;
 mod valid_parenthesis_string;
 mod remove_duplicate_letters;
+mod boats_to_save_people;
