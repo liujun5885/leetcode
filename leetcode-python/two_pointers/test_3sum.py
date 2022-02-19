5bc124b9d92a19bf77e04cb37b47010793a2b107
liujun <liujun@rxthinking.com>
2022-02-19 10:07:18 +0800

    add reverse_integer.py

---
+++ leetcode-python/two_pointers/test_3sum.py
@@ -0,0 +1,58 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        target = 0
+        ans = []
+        nums = sorted(nums)
+        n = len(nums)
+
+        for first in range(n):
+            if first > 0 and nums[first] == nums[first - 1]:
+                first += 1
+                continue
+
+            third = n - 1
+            for second in range(first + 1, n):
+                if second > first + 1 and nums[second] == nums[second - 1]:
+                    second += 1
+                    continue
+
+                left = target - nums[first] - nums[second]
+                while second < third and nums[third] > left:
+                    third -= 1
+
+                if second == third:
+                    break
+
+                if left == nums[third]:
+                    ans.append([nums[first], nums[second], nums[third]])
+
+        return ans
+
+
+class TestSolution:
+    def test_case01(self):
+        nums = [-1, 0, 1, 2, -1, -4]
+        actual = Solution().threeSum(nums)
+        expected = [[-1, -1, 2], [-1, 0, 1]]
+        assert sorted(actual) == sorted(expected)
+
+    def test_case02(self):
+        nums = []
+        actual = Solution().threeSum(nums)
+        expected = []
+        assert sorted(actual) == sorted(expected)
+
+    def test_case03(self):
+        nums = [0]
+        actual = Solution().threeSum(nums)
+        expected = []
+        assert sorted(actual) == sorted(expected)
+
+    def test_case04(self):
+        nums = [0, 0, 0, 1, -1]
+        actual = Solution().threeSum(nums)
+        expected = [[0, 0, 0], [-1, 0, 1]]
+        assert sorted(actual) == sorted(expected)
---
+++ leetcode-python/two_pointers/test_3sum_closest.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        return 0
+
+
+class TestSolution:
+    def test_case(self):
+        nums = [-1, 2, 1, -4]
+        target = 1
+        actual = Solution().threeSumClosest(nums, target)
+        expected = 2
+        assert actual == expected
