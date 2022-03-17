c7c281c966534a718b58353f5a6cd595c61a9fad
liujun <liujun@rxthinking.com>
2022-03-17 11:40:37 +0800

    update

--- leetcode-python/backtracking/maximum_length_of_a_concatenated_string_with_unique_characters.py
+++ leetcode-python/backtracking/maximum_length_of_a_concatenated_string_with_unique_characters.py
@@ -5,12 +5,42 @@ from typing import List
 
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
-        return 0
+        mask_list = []
+        for s in arr:
+            mask = 0
+            for i in s:
+                idx = ord(i) - ord('a')
+                if (mask >> idx) & 1:
+                    mask = 0
+                    break
+                else:
+                    mask |= 1 << idx
+            if mask > 0:
+                mask_list.append(mask)
+
+        ans = 0
+
+        def traceback(n, m):
+            nonlocal ans
+            if n == len(mask_list):
+                ans = max(ans, bin(m).count('1'))
+                return
+
+            if mask_list[n] & m == 0:
+                traceback(n + 1, mask_list[n] | m)
+            traceback(n + 1, m)
+
+        traceback(0, 0)
+
+        return ans
 
 
-class TestCases:
+import unittest
+
+
+class TestCases(unittest.TestCase):
     def test_case_01(self):
         arr = ["un", "iq", "ue"]
         actual = Solution().maxLength(arr)
         expected = 4
-        assert actual == expected
+        self.assertEqual(actual, expected)
