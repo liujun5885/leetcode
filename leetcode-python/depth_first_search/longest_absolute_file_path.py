d1f5f70d1b24f075856e2742e402f7b3e645b889
liujun <liujun@rxthinking.com>
2022-03-21 19:39:07 +0800

    update

--- leetcode-python/depth_first_search/longest_absolute_file_path.py
+++ leetcode-python/depth_first_search/longest_absolute_file_path.py
@@ -1,6 +1,7 @@
 # https://leetcode-cn.com/problems/longest-absolute-file-path/
 
 from unittest import TestCase
+import os
 
 
 class Solution:
@@ -13,12 +14,28 @@ class Solution:
                 if j != '\t':
                     break
                 level += 1
-            level_input.append((level, i.strip()))
+            level_input.append((level, i.replace('\t', '')))
 
-        def dfs(s: str):
-            return
+        level = 0
+        folder = []
+        for i in level_input:
+            if '.' in i[1]:
+                path_list.append(os.path.join(*(folder[:i[0]] + [i[1]])))
+                continue
+            if i[0] == level:
+                level += 1
+                folder.append(i[1])
+            elif i[0] < level:
+                # path_list.append('/'.join(folder))
+                level = i[0]
+                folder = folder[0:i[0]]
+                folder.append(i[1])
+                level += 1
 
-        return len(path_list)
+        if len(path_list) == 0:
+            return 0
+
+        return max(map(lambda x: len(x), path_list))
 
 
 class TestCases(TestCase):
@@ -31,5 +48,23 @@ class TestCases(TestCase):
     def test_02(self):
         input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
         expected = 32
+        actual = Solution().lengthLongestPath(input)
+        self.assertEqual(actual, expected)
+
+    def test_03(self):
+        input = "a"
+        expected = 0
+        actual = Solution().lengthLongestPath(input)
+        self.assertEqual(actual, expected)
+
+    def test_04(self):
+        input = "file1.txt\nfile2.txt\nlongfile.txt"
+        expected = 12
+        actual = Solution().lengthLongestPath(input)
+        self.assertEqual(actual, expected)
+
+    def test_05(self):
+        input = "dir\n        file.txt"
+        expected = 16
         actual = Solution().lengthLongestPath(input)
         self.assertEqual(actual, expected)
