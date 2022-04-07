36bf606f4c8fe40788048021cb340002be1e74e3
liujun <liujun@rxthinking.com>
2022-04-07 13:51:32 +0800

    update

--- leetcode-python/breadth_first_search/snakes_and_ladders.py
+++ leetcode-python/breadth_first_search/snakes_and_ladders.py
@@ -1,13 +1,42 @@
 # https://leetcode-cn.com/problems/snakes-and-ladders/
-
+from collections import deque
 from typing import List
 from unittest import TestCase
 
 
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        ans = 0
-        return ans
+        n = len(board)
+        visited = set()
+        q = deque([(1, 0)])
+
+        def id_to_x_y(id: int):
+            x = n - (id - 1) // n - 1
+            if (n - 1 - x) % 2 == 0:
+                y = (id - 1) % n
+            else:
+                y = n - 1 - (id - 1) % n
+            return x, y
+
+        while len(q) > 0:
+            p = q.popleft()
+            for i in range(1, 7):
+                idx = p[0] + i
+                if idx > n * n:
+                    break
+
+                x, y = id_to_x_y(idx)
+                if board[x][y] != -1:
+                    idx = board[x][y]
+
+                if idx == n * n:
+                    return p[1] + 1
+
+                if idx not in visited:
+                    visited.add(idx)
+                    q.append((idx, p[1] + 1))
+
+        return -1
 
 
 class TestCases(TestCase):
@@ -21,5 +50,13 @@ class TestCases(TestCase):
             [-1, 15, -1, -1, -1, -1]
         ]
         expected = 4
+        actual = Solution().snakesAndLadders(board)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        board = [
+            [-1, -1], [-1, 3]
+        ]
+        expected = 1
         actual = Solution().snakesAndLadders(board)
         self.assertEqual(expected, actual)
