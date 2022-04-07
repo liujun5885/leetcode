a61f45b3f79ca01aa8288469c30c45b907d2ed7a
liujun <liujun@rxthinking.com>
2022-04-07 13:55:38 +0800

    update

--- leetcode-python/breadth_first_search/snakes_and_ladders.py
+++ leetcode-python/breadth_first_search/snakes_and_ladders.py
@@ -11,6 +11,7 @@ class Solution:
         q = deque([(1, 0)])
 
         def id_to_x_y(id: int):
+            # convert id to row and column.
             x = n - (id - 1) // n - 1
             if (n - 1 - x) % 2 == 0:
                 y = (id - 1) % n
@@ -26,15 +27,15 @@ class Solution:
                     break
 
                 x, y = id_to_x_y(idx)
-                if board[x][y] != -1:
+                if board[x][y] != -1:  # if it's a ladder or a snake, next is board[x][y]
                     idx = board[x][y]
 
-                if idx == n * n:
+                if idx == n * n:  # if next is n * n, it's the end, move = current move + 1
                     return p[1] + 1
 
                 if idx not in visited:
                     visited.add(idx)
-                    q.append((idx, p[1] + 1))
+                    q.append((idx, p[1] + 1))  # move to next
 
         return -1
 
