d23c34c0a7a71227973ebd807dfc52c1ed678e1d
liujun <liujun@rxthinking.com>
2022-04-10 15:55:22 +0800

    update

--- leetcode-python/dynamic_programming/best_time_to_buy_and_sell_stock_iii.py
+++ leetcode-python/dynamic_programming/best_time_to_buy_and_sell_stock_iii.py
@@ -6,8 +6,15 @@ from unittest import TestCase
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ans = 0
-        return ans
+        b1 = b2 = -prices[0]
+        s1 = s2 = 0
+        for p in prices:
+            b1 = max(b1, -p)
+            s1 = max(s1, b1 + p)
+            b2 = max(b2, s1 - p)
+            s2 = max(s2, b2 + p)
+
+        return s2
 
 
 class TestCases(TestCase):
